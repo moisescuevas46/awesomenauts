cd538c1ae03a9b86e7315fbea553e7db1175d1b8
Moises A. CUevas <moisescuevas46@gmail.com>
2015-01-23 11:38:17 -0800

    up to video 6, isnt woring though, need to fix it

--- js/screens/play.js
+++ js/screens/play.js
@@ -8,6 +8,8 @@ game.PlayScreen = me.ScreenObject.extend({
 
 		me.levelDirector.loadLevel("level01")
 
+		var player = me.pool.pull("player", 0, 420, []);
+		me.game.world.addChild("player", 5);
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
