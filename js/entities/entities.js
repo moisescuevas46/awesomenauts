365bbb8aef1862a1b6929924a019ed3a6518d791
Moises A. CUevas <moisescuevas46@gmail.com>
2015-02-13 14:25:40 -0800

    video 20 done and almsot finished with video 21

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -185,7 +185,7 @@ game.EnemyBaseEntity = me.Entity.extend({
 	},
 
 	update:function(delta){
-		if(this.health<=0){
+		if(this.health<=2){
 			this.broken = true;
 			this.renderable.setCurrentAnimation("broken");
 		}
@@ -215,16 +215,41 @@ this._super(me.Entity, 'init', [x,y, {
 	getShape: function(){
 		return(new me.Rect(0, 0, 32, 64)).toPolygon();
 	}
-}])
+}]);
 this.health = 10;
 this.alwaysUpdate = true;
-
+//Lets us know if the enemy is curretly attacking.
+this.attacking = false;
+this.now = new Date().getTime();
 this.body.setVelocity(3,20);
 this.type = "EnemyCreep";
 
 this.renderable.addAnimation("walk",[3,4,5],80);
 this.renderable.setCurrentAnimation("walk");
 	},
+	update: function(delta){
+		this.now = new Date().getTime();
+
+		this.body.vel.x -= this.body.accel.x * me.timer.tick;
+		me.collision.check(this, true, this.collideHandler.bind(this), true);
+		this.body.update(delta);
+		this._super(me.Entity, "update", [delta]);
+
+		return true;
+	},
+	collideHandler: function(response){
+		if (response.b.type==='PlayerBase') {
+			this.attacking=true;
+			this.lastAttacking=this.now;
+			this.body.vel.x = 0;
+			this.pos.x = this.pos.x + 1;
+			if((this.now-this.lastMit >= 1000)){
+				this.lastMit = this.now;
+				response.b.loseHealth(1);
+			}
+
+		};
+	}
 });
 
 
